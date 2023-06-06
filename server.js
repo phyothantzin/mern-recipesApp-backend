3bbb564db69fa7b3dbf15b1a657fbaa63b19b038
phyothantzin <105695346+phyothantzin@users.noreply.github.com>
2023-06-06 15:11:39 +0630

    Update server.js

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ connectDB()
 const port = process.env.PORT || 5500
 
 const app = express()
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use(cors())
 
 app.use('/api/users', userRoutes)
 app.use('/api/recipes', recipesRoutes)

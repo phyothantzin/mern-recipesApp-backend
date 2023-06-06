afab8a50bdd7ab2c558e6f71364a5c8bb8cfa435
phyothantzin <105695346+phyothantzin@users.noreply.github.com>
2023-06-06 15:14:23 +0630

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+  origin: 'https://therecipesapp.onrender.com'
+}))
 
 app.use('/api/users', userRoutes)
 app.use('/api/recipes', recipesRoutes)

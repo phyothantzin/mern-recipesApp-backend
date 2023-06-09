6e536f6c86a4f7f1705b2aa89a7c37a39da21aa2
phyothantzin <105695346+phyothantzin@users.noreply.github.com>
2023-06-09 11:32:33 +0630

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", 'https://therecipesapp.onrender.com');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json,content-type,multipart/form-data');
     next();
 });
 app.get('/', (req, res) => res.status(200).send('Server running'))
